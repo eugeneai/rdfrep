79bb7dffa8f8b0720cc89170f8a6e87e1678eabf
paskalkris <paskal@istu.edu>
2014-03-23 13:12:44 +0900

    The object definition added.

--- rdfrep/storage.py
+++ rdfrep/storage.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import rdflib
 import rdflib.store as store
-from rdflib.namespace import RDF
-from rdflib.namespace import FOAF
+from rdflib import Namespace
+from rdflib.namespace import FOAF, RDF
+
+PERSON_PT = rdflib.Literal('person.pt')
+dc = Namespace('http://purl.org/dc/elements/1.1/')
+people = Namespace('http://www.w3.org/People/Berners-Lee/card#')
+CLASS = rdflib.URIRef('http://www.exaple.com/test/class')
+PERSON_CLASS = rdflib.URIRef('http://www.exaple.com/test/class#person')
+PT = rdflib.URIRef('http://www.exaple.com/test/pt')
+HTML=rdflib.URIRef('http://www.w3.org/DesignIssues/Overview.html')
 
 class RDFStorage():
     def load_graph(self,store="KyotoCabinet",path="/home/paskal/python/rdfstore"):
@@ -12,6 +20,9 @@ class RDFStorage():
         
         if len(g)==0:
             g.parse("http://www.w3.org/People/Berners-Lee/card")
+            for p in g.subjects(RDF.type, FOAF.Person):
+	        g.add((p, CLASS, PERSON_CLASS))
+	    g.add((PERSON_CLASS, PT, PERSON_PT))
             g.commit()
             print "A graph has been obtained and loaded."
             
@@ -20,23 +31,36 @@ class RDFStorage():
 
     def __init__(self,store="KyotoCabinet",path="/home/paskal/python/rdfstore"):
         self.graph=self.load_graph(store,path)
-          
+    
     def close_graph(self):
         self.graph.close()
+        return True
+    
+    def toRDF(self, string):
+        return rdflib.URIRef(string)
+    
+    def get_pt(self, obj):
+        obj_class = self.graph.value(obj, CLASS)
+        return self.graph.value(obj_class, PT)
     
     def test(self):
-        Type=rdflib.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-        HTML=rdflib.URIRef('http://www.w3.org/DesignIssues/Overview.html')
-        for s,p,o in self.graph.triples((HTML, None, None)):
-            print "-----",(p,o)
+        #print self.graph.serialize(format='n3')
+        self.graph.remove((None,None,None))
+        #print len(self.graph)
+        #Type=rdflib.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+        #HTML=rdflib.URIRef('http://www.w3.org/DesignIssues/Overview.html')
+        #for s,p,o in self.graph.triples((PERSON, None, None)):
+        #    print "-----",(p,o)
 
-    def title(self):
-        HTML=rdflib.URIRef('http://www.w3.org/DesignIssues/Overview.html')
-        Title=rdflib.URIRef('http://purl.org/dc/elements/1.1/title')
-        for obj in self.graph.objects(HTML,Title):
-	  return obj
+    def title(self, name):
+	return self.graph.value(HTML,name)
 
 if __name__=="__main__":
     g = RDFStorage()
-    print g.title()
+    #g.test()
+    #g.graph.commit()
+    #print g.title('http://purl.org/dc/elements/1.1/title')
+    print g.get_pt(people.i)
+    #print g.get_class(people.i)
+    #print ns.isclass
     g.close_graph()
--- rdfrep/views.py
+++ rdfrep/views.py
@@ -1,16 +1,20 @@
 from pyramid.view import view_config
-from storage import RDFStorage
-from pyramid.renderers import render_to_response
+from storage import RDFStorage, people
+from pyramid.renderers import render_to_response, get_renderer
 
 PERSON_PT='templates/person.pt'
+BASE_PT='templates/base.pt'
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'rdfrep'}
 
-@view_config(route_name='person', renderer=PERSON_PT)
-def my_view(request):
+@view_config(route_name='person')
+def pers_view(request):
+    #base = get_renderer(BASE_PT).implementation()
     g = RDFStorage()
-    return render_to_response(PERSON_PT,
-                              {'graph':g,},
+    obj = g.toRDF('http://www.w3.org/People/Berners-Lee/card#i')
+    return render_to_response(BASE_PT,
+                              {'graph':g,
+			       'person':obj,},
                               request=request)
